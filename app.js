784ff77811507471dc43a3d877c27c3d1b6db9c7
Jcook894 <Jcook894@icloud.com>
2016-10-14 22:11:27 -0400

    Need to fix all bouncing markers when you click one and also need to fix the color changing markers.

--- app.js
+++ app.js
@@ -7,126 +7,181 @@ var markers = [];
 
 
 
+
 <!-- the view -->
 
 
 function initMap(){
-  var style = 
-           [
-             {elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},
-             {elementType: 'labels.text.fill', stylers: [{color: '#523735'}]},
-             {elementType: 'labels.text.stroke', stylers: [{color: '#f5f1e6'}]},
-             {
-               featureType: 'administrative',
-               elementType: 'geometry.stroke',
-               stylers: [{color: '#c9b2a6'}]
-             },
-             {
-               featureType: 'administrative.land_parcel',
-               elementType: 'geometry.stroke',
-               stylers: [{color: '#dcd2be'}]
-             },
-             {
-               featureType: 'administrative.land_parcel',
-               elementType: 'labels.text.fill',
-               stylers: [{color: '#ae9e90'}]
-             },
-             {
-               featureType: 'landscape.natural',
-               elementType: 'geometry',
-               stylers: [{color: '#dfd2ae'}]
-             },
-             {
-               featureType: 'poi',
-               elementType: 'geometry',
-               stylers: [{color: '#dfd2ae'}]
-             },
-             {
-               featureType: 'poi',
-               elementType: 'labels.text.fill',
-               stylers: [{color: '#93817c'}]
-             },
-             {
-               featureType: 'poi.park',
-               elementType: 'geometry.fill',
-               stylers: [{color: '#a5b076'}]
-             },
-             {
-               featureType: 'poi.park',
-               elementType: 'labels.text.fill',
-               stylers: [{color: '#447530'}]
-             },
-             {
-               featureType: 'road',
-               elementType: 'geometry',
-               stylers: [{color: '#f5f1e6'}]
-             },
-             {
-               featureType: 'road.arterial',
-               elementType: 'geometry',
-               stylers: [{color: '#fdfcf8'}]
-             },
-             {
-               featureType: 'road.highway',
-               elementType: 'geometry',
-               stylers: [{color: '#f8c967'}]
-             },
-             {
-               featureType: 'road.highway',
-               elementType: 'geometry.stroke',
-               stylers: [{color: '#e9bc62'}]
-             },
-             {
-               featureType: 'road.highway.controlled_access',
-               elementType: 'geometry',
-               stylers: [{color: '#e98d58'}]
-             },
-             {
-               featureType: 'road.highway.controlled_access',
-               elementType: 'geometry.stroke',
-               stylers: [{color: '#db8555'}]
-             },
-             {
-               featureType: 'road.local',
-               elementType: 'labels.text.fill',
-               stylers: [{color: '#806b63'}]
-             },
-             {
-               featureType: 'transit.line',
-               elementType: 'geometry',
-               stylers: [{color: '#dfd2ae'}]
-             },
-             {
-               featureType: 'transit.line',
-               elementType: 'labels.text.fill',
-               stylers: [{color: '#8f7d77'}]
-             },
-             {
-               featureType: 'transit.line',
-               elementType: 'labels.text.stroke',
-               stylers: [{color: '#ebe3cd'}]
-             },
-             {
-               featureType: 'transit.station',
-               elementType: 'geometry',
-               stylers: [{color: '#dfd2ae'}]
-             },
-             {
-               featureType: 'water',
-               elementType: 'geometry.fill',
-               stylers: [{color: '#b9d3c2'}]
-             },
-             {
-               featureType: 'water',
-               elementType: 'labels.text.fill',
-               stylers: [{color: '#92998d'}]
-             }
-           ];
+  var style =
+  [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#242f3e"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#746855"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#242f3e"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.locality",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#d59563"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#d59563"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#263c3f"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#6b9a76"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#38414e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#212a37"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9ca5b3"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#746855"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#1f2835"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#f3d19c"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#2f3948"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#d59563"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#17263c"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#515c6d"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#17263c"
+      }
+    ]
+  }
+]
+
 
   map = new google.maps.Map(document.getElementById("map"),{
     center: {lat: 41.7637, lng:  -72.6851},
     zoom: 18,
-    styles: style
+    styles: style,
+    mapTypeControl: false,
+
 
   });
 
@@ -142,6 +197,10 @@ function initMap(){
   var largeInfowindow = new google.maps.InfoWindow();
   var bounds = new google.maps.LatLngBounds();
 
+  //Styles the markers a too when you hover over it it changes color.
+  var defaultMarker = makeMarkerIcon("0091ff");
+  var highlightMark = makeMarkerIcon("FFFF4");
+
   for(var i = 0; i < locations.length;i++){
     // gets the positions & titles from the location array.
     var position = locations[i].location;
@@ -152,15 +211,25 @@ function initMap(){
       position: position,
       title: title,
       id: i,
-      animation: google.maps.Animation.DROP
+      animation: google.maps.Animation.DROP,
+      icon: defaultMarker
+
+
 
     });
+    marker.addListener('click', toggleBounce);
+
+    marker.addListener('mouseover', function(){
+      this.setIcon(highlightMark);
+    });
+
+    marker.addListener('mouseout', function(){
+      this.setIcon(defaultMarker);
+    });
+
+
   //pushes the marker above to the markers array
     markers.push(marker);
-  //extends the bounds to the markers.
-
-
-
 // opens infowindow when marker is clicked
   marker.addListener('click', function(){
     populateInfoWindow(this, largeInfowindow);
@@ -203,6 +272,7 @@ function populateInfoWindow(marker, infowindow){
 
         bounds.extend(markers[i].position);
 
+
     }
     map.fitBounds(bounds);
   }
@@ -217,4 +287,29 @@ function populateInfoWindow(marker, infowindow){
 
   };
 };
-};
+
+//When you click the marker, it starts bouncing untill you click it again.
+function toggleBounce(){
+
+  for(var i = 0; i < locations.length;i++){
+
+        if(markers[i].getAnimation() !== null){
+          markers[i].setAnimation(null);
+        }
+        else{
+          markers[i].setAnimation(google.maps.Animation.BOUNCE);
+        }
+    };
+  };
+  function makeMarkerIcon(markerColor) {
+          var markerImage = new google.maps.MarkerImage('http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=1.15|0|'+ markerColor + '|40|_|%E2%80%A2',
+
+            new google.maps.Size(21, 34),
+            new google.maps.Point(0, 0),
+            new google.maps.Point(10, 34),
+            new google.maps.Size(21,34));
+          return markerImage;
+
+        };
+
+  };
